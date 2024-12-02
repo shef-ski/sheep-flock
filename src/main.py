98407c33a6ca5c4ad2209f8f8b5f9354df7fa145
Cus, Tibor <cus.tibor@outlook.com>
2024-12-02 22:16:36 +0100

    add initial project setup

--- src/main.py
+++ src/main.py
@@ -1,3 +1,23 @@
 
-print("i like sheep")
+import pygame 
+import sys
+
+from enviroment import Enviroment
+
+def main():
+    print("Hello world")
+
+if __name__ == "__main__":
+    env = Enviroment((800,800), 10, 2)
+    paused = False
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_SPACE:
+                    paused = not paused
+        if not paused:
+            env.draw()
 
