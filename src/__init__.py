67b4510f4150502973d6c4cc90410900c01c4454
Leon <leon.olszewski99@gmail.com>
2024-12-13 11:07:30 +0100

    changed logging filepath to work with Windows OS

--- src/__init__.py
+++ src/__init__.py
@@ -4,4 +4,6 @@ from datetime import datetime
 
 n_sheep = os.getenv("N_SHEEP",0)
 n_dog = os.getenv("N_DOG",0)
-LOG_OUTPUTFILE = Path(__file__).parent.parent / "logs" / f"durations_{n_sheep}_{n_dog}_{datetime.now()}"
+
+timestamp_now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+LOG_OUTPUTFILE = Path(__file__).parent.parent / "logs" / f"durations_{n_sheep}_{n_dog}_{timestamp_now}"
